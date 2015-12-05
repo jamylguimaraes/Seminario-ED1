6cd3c95fa257c04eea0a8921126617ff23635808
Jamyl Guimarães <jamyl.cb@gmail.com>
2015-12-05 17:26:43 -0300

    More tests

--- src/local/app/Main.java
+++ src/local/app/Main.java
@@ -13,12 +13,13 @@ public class Main
 	public static void main( String[] args ) throws Exception
 	{
 		tree  = new SplayTree();
+		tree.insert(6);
 		tree.insert(5);
 		tree.insert(8);
 		tree.insert(2);
 		tree.insert(4);
 		tree.display();
-		System.out.println("Inserido: 5,8,2,4");
+		System.out.println("Inserido: 6,5,8,2,4");
 		tree.find(5);
 		tree.display();
 		System.out.println("Find: 5");
