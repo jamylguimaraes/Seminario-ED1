77ff8c5c1127d34545fed10291e11372de4a9224
Jamyl Guimarães <jamyl.cb@gmail.com>
2015-12-05 17:23:28 -0300

    Alterações no Main

--- src/local/app/Main.java
+++ src/local/app/Main.java
@@ -18,9 +18,12 @@ public class Main
 		tree.insert(2);
 		tree.insert(4);
 		tree.display();
+		System.out.println("Inserido: 5,8,2,4");
 		tree.find(5);
 		tree.display();
+		System.out.println("Find: 5");
 		tree.remove(5);
 		tree.display();
+		System.out.println("Remove: 5");
 	}
 }
